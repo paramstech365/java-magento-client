74afa2a5f805459e9aade99055d943c3a976fd5f
Madhavi <madhavi@paramstech.com>
2020-02-21 20:11:26 -0500

    commit

--- src/test/java/com/github/chen0040/magento/MagentoClientInventoryUnitTest.java
+++ src/test/java/com/github/chen0040/magento/MagentoClientInventoryUnitTest.java
@@ -16,15 +16,17 @@ public class MagentoClientInventoryUnitTest {
 
    private static final Logger logger = LoggerFactory.getLogger(MagentoClientInventoryUnitTest.class);
    @Test
-   public void test_getStockItems(){
-      String productSku = "product_dynamic_571";
+   public static void test_getStockItems(){
+      String productSku = "MDM_1_10001";
 
       MagentoClient client = new MagentoClient(Mediator.url);
       client.loginAsAdmin(Mediator.adminUsername, Mediator.adminPassword);
-      logger.info("stock item: {}", JSON.toJSONString(client.inventory().getStockItems(productSku), SerializerFeature.PrettyFormat));
+      logger.info("stock item: {}", JSON.toJSONString(client.products().getProductBySku(productSku), SerializerFeature.PrettyFormat));
+   //   logger.info("stock item: {}", JSON.toJSONString(client.inventory().getStockItems(productSku), SerializerFeature.PrettyFormat));
+      
 
-      productSku = "B203-SKU";
-      logger.info("stock item: {}", JSON.toJSONString(client.inventory().getStockItems(productSku), SerializerFeature.PrettyFormat));
+  //    productSku = "B203-SKU";
+  //    logger.info("stock item: {}", JSON.toJSONString(client.inventory().getStockItems(productSku), SerializerFeature.PrettyFormat));
    }
 
    @Test
@@ -36,8 +38,12 @@ public class MagentoClientInventoryUnitTest {
 
       productSku = "B203-SKU";
       StockItems si = client.inventory().getStockItems(productSku);
-      si.setQty(2);
+     // si.setQty(2);
       String stockId = client.inventory().saveStockItems(productSku, si);
       logger.info("stock item saved: {}", stockId);
+   }
+   
+   public static void main(String args[]) {
+	   test_getStockItems();
    }
 }
--- src/test/java/com/github/chen0040/magento/Mediator.java
+++ src/test/java/com/github/chen0040/magento/Mediator.java
@@ -6,9 +6,9 @@ package com.github.chen0040.magento;
  */
 public class Mediator {
 
-   public static final String url = "http://magento2.demo.ubertheme.com/";
+   public static final String url = "http://ec2-3-6-203-109.ap-south-1.compute.amazonaws.com/";
    public static final String customerUsername = "roni_cost@example.com";
    public static final String customerPassword = "roni_cost@example.com";
-   public static final String adminUsername = "megamenu";
-   public static final String adminPassword = "megamenu@123";
+   public static final String adminUsername = "admin";
+   public static final String adminPassword = "admin123";
 }
